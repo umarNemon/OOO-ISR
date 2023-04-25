170ac0f29e500152f1d775f5483beb7dcc9f1e43
umarNemon <107072941+umarNemon@users.noreply.github.com>
2023-04-25 23:59:43 +0300

    Slider

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
